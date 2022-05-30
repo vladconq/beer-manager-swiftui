7a0fc804381311a0fddf0c4659a790aaa0a11ceb
vladconq <vladconq@gmail.com>
2022-05-30 18:38:01 +0400

    Initial Commit

---
+++ BeerManagerSwiftUI/BeerManagerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeerManagerSwiftUIApp.swift
+//  BeerManagerSwiftUI
+//
+//  Created by mainuser on 30.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct BeerManagerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
